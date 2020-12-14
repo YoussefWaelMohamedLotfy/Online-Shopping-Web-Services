b9d1b6bfeaa302b4d411dcb7a943a8a30c1c7251
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2020-12-15 00:06:34 +0200

    Added new project

---
+++ Online Shopping Service/Online Shopping Service/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Shopping_Service.Startup))]
+namespace Online_Shopping_Service
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

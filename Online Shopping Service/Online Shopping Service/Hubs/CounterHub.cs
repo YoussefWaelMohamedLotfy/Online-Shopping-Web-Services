8e8e1a51cb93c2e5a6508611a10f8916b24a9fef
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-03-19 15:39:51 +0200

    Added a real-time global online user counter in Home Page

---
+++ Online Shopping Service/Online Shopping Service/Hubs/CounterHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Online_Shopping_Service.Hubs
+{
+    public class CounterHub : Hub
+    {
+        static long counter = 0;
+
+        public override Task OnConnected()
+        {
+            counter++;
+            Clients.All.UpdateCount(counter);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            counter--;
+            Clients.All.UpdateCount(counter);
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
